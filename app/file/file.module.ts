23a16250149fe4f84d565dcb89970ca10f6f62b0
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-01-31 16:14:32 -0500

    up

--- app/file/file.module.ts
+++ app/file/file.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
 
 import { UploadComponent } from './upload.component';
 import { FileRouting } from './file.routing';
 @NgModule({
     imports: [
+        BrowserModule,
         CommonModule,
         FileRouting
     ],
     declarations: [
-       UploadComponent 
+       UploadComponent,FileSelectDirective 
     ]
 })
 
