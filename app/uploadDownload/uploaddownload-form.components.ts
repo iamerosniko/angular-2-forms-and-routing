2a903e1f1075b45bdcb3a8aa9c4353354990146f
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-01-31 10:53:57 -0500

    up

---
+++ app/uploadDownload/uploaddownload-form.components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'car-form.component.html'
+})
+
+export class CarFormComponent {
+    
+}
